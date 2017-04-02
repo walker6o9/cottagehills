dd00460791b1a6c2126bd2409f336b3982419c23
AWS Ubuntu Server <cottagesmtp@gmail.com>
2017-04-02 16:51:03 -0700

    Fix for sides

--- sites/all/modules/dc_ajax_add_cart/templates/dc-ajax-add-to-cart-message.tpl.php
+++ sites/all/modules/dc_ajax_add_cart/templates/dc-ajax-add-to-cart-message.tpl.php
@@ -124,7 +124,7 @@
       var t = document.getElementById("ajax-shopping-cart-table"), // This have to be the ID of the table, not the tag
           d = t.getElementsByTagName("tr")[t.getElementsByTagName("tr").length-1];//['ajax-cart-row'],
           r = d.getElementsByTagName("td")['name'].innerHTML;
-          d.getElementsByTagName("td")['name'].innerHTML= " "+ "<? echo explode("<br>", $product->title)[0]; ?>" +"<br><b>Side: </b>"+ this.options[this.selectedIndex].text;
+          d.getElementsByTagName("td")['name'].innerHTML= " "+ "<? /*dsm($product->title);echo explode("<br>",*/ $product->title/*)[0]*/; ?>" +"<br><b>Side: </b>"+ this.options[this.selectedIndex].text;
       var ArrayOfSides = new Array();
       var ArrayOfLids = new Array();
       var temp =  new Array();
@@ -141,7 +141,7 @@
           {
               ArrayOfSides = ArrayOfSides.concat(temp[i]);
           }
-          ArrayOfSides.push('<?php echo /*html_entity_decode(*/"<b>".explode(":",$product_name)[0].": </b>".htmlspecialchars(explode(":",$product_name)[1])/*)*/;?>"+"<br><b>Side: </b>'+ this.options[this.selectedIndex].text);
+          ArrayOfSides.push('<?php echo html_entity_decode("<b>".explode(":",$product_name)[0].": </b>".explode(":",$product_name)[1]);?>'+'<br><b>Side: </b>'+ this.options[this.selectedIndex].text);
           updatedString = JSON.stringify(ArrayOfSides).replace(/'/g, "\\'");//replace(/('[a-zA-Z0-9\s]+\s*)'(\s*[a-zA-Z0-9\s]+')/g,"$1\\\'$2");
 		  document.cookie =  "productname = " + encodeURI(updatedString);
           
